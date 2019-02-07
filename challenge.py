d234a154da43bb932e07eb470286f6376706037f
Finneyyy <eoin.finney@gmai.com>
2019-02-07 21:33:38 +0000

    17:50:00 07/02/19 Thursday evening labs with mc

--- challenge.py
+++ challenge.py
@@ -1,1 +1,7 @@
-a=1
+# Rot5, Rot13, and Rot47
+
+rot13 = str.maketrans( "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz", "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm")
+print(str.translate("What did you use to hash this", rot13))
+
+# def rot47:
+
